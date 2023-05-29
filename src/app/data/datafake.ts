92e9eea50ff02b03439a6efeb8a3243c3e9d9c82
Roberto <robertosilva.2@hotmail.com>
2023-05-28 22:40:14 -0300

    conteudo dinâmico

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,17 @@
+
+export const dataFake =[
+    {
+      "id":"1",
+      "title": "Nova bmw s1000rr",
+      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+      "photoCover": "https://motonewsbrasil.com/wp-content/uploads/2022/09/bmw-s-1000-rr-2023-estatica.jpg"
+    },
+    {
+      "id":"2",
+      "title": "MOtor mais potente bmw s1000rr",
+      "description": "Olá mundo",
+      "photoCover": "https://motonewsbrasil.com/wp-content/uploads/2020/09/bmw-m-1000-rr-2021-1.jpg"
+    }
+]
+
